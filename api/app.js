74924bc2b9ad5a590661adac699af78c21bfad25
onubianca <biancaonu07@gmail.com>
2026-01-05 19:50:59 +0200

    commit 1

---
+++ api/app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    next();
+});
+
+app.use(express.static('web'));
+
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).json({ message: 'Internal Server Error' });
+});
+
+export default app;
